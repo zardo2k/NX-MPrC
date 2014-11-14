bc127e9e7ba8d0e59319114531468426328d29bc
DJ Zardo <zardo2k@gmail.com>
2014-11-13 20:23:17 -0800

    CSS styling

--- phone/www/js/SideBarCtrl.js
+++ phone/www/js/SideBarCtrl.js
@@ -1,17 +1,19 @@
 angular.module('SideBarModule', ['FbClustersSrvModule'])
   .controller('SideBarCtrl',
     ['$scope', 'FbClustersSrv', 'Resources',
-      '$ionicSideMenuDelegate', '$state',
+      '$ionicSideMenuDelegate', '$state', '$ionicNavBarDelegate',
       function($scope, FbClustersSrv, Resources,
-               $ionicSideMenuDelegate, $state) {
+               $ionicSideMenuDelegate, $state, $ionicNavBarDelegate) {
         $scope.clusters = FbClustersSrv.getAll();
 
         $scope.onSelectCluster = function(cluster) {
           if(typeof cluster === 'string') {
             Resources.currentSelectCluster.name = cluster;
             Resources.currentSelectCluster.id = null;
+            $ionicNavBarDelegate.$getByHandle('myNav').setTitle('ALL')
             $state.go('home');
           } else {
+            $ionicNavBarDelegate.$getByHandle('myNav').setTitle('newTitle')
             Resources.currentSelectCluster.name = cluster.name;
             Resources.currentSelectCluster.id = cluster.clusterUuid;
             $state.go('clusters', {id: cluster.clusterUuid});
